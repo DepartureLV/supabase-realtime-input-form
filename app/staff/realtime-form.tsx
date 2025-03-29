c08701d3e263674c46915a1fec7f78967fe8e402
Nattawat Pitikomon <nattawat.arch@gmail.com>
2025-03-29 23:46:30 +0700

    Feat: motion

--- app/staff/realtime-form.tsx
+++ app/staff/realtime-form.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import PersonalForm from "@/components/personal-form";
 import { OnlineStatus } from "@/type/online_status";
 import { CircleCheck } from "lucide-react";
+import { AnimatePresence, motion } from "framer-motion";
 
 type PresenceInfo = {
   presence_ref: string;
@@ -104,38 +105,54 @@ export default function RealtimeForm({
 }
 
 function OnlineBadge({ status }: { status: OnlineStatus }) {
-  if (status === "inactive") {
-    return (
-      <div className="flex gap-2 items-center text-yellow-400">
-        <div className="relative h-6 w-6">
-          <div className="absolute h-full w-full bg-yellow-400 rounded-full z-10" />
-          <div className="absolute h-full w-full bg-yellow-400 rounded-full animate-ping" />
-        </div>
-        <span>{status}</span>
-      </div>
-    );
-  }
+  return (
+    <AnimatePresence>
+      <div className="w-fit h-8">
+        {status === "inactive" && (
+          <motion.div
+            key="inactive"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            className="flex gap-2 items-center text-yellow-400"
+          >
+            <div className="relative h-6 w-6">
+              <div className="absolute h-full w-full bg-yellow-400 rounded-full z-10" />
+              <div className="absolute h-full w-full bg-yellow-400 rounded-full animate-ping" />
+            </div>
+            <span>{status}</span>
+          </motion.div>
+        )}
 
-  if (status === "submit") {
-    return (
-      <div className="flex gap-2 items-center">
-        <CircleCheck className="text-cyan-700" />
-        <span>{status}</span>
-      </div>
-    );
-  }
+        {status === "submitted" && (
+          <motion.div
+            key="submit"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            className="flex gap-2 items-center"
+          >
+            <CircleCheck className="text-cyan-700" />
+            <span>{status}</span>
+          </motion.div>
+        )}
 
-  if (status === "editing") {
-    return (
-      <div className="flex gap-2 items-center">
-        <div className="relative h-6 w-6">
-          <div className="absolute h-full w-full bg-green-700 rounded-full z-10" />
-          <div className="absolute h-full w-full bg-green-700 rounded-full animate-ping" />
-        </div>
-        <span>{status}</span>
+        {status === "editing" && (
+          <motion.div
+            key="editing"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.3 }}
+            className="flex gap-2 items-center text-green-700"
+          >
+            <div className="relative h-6 w-6">
+              <div className="absolute h-full w-full bg-green-700 rounded-full z-10" />
+              <div className="absolute h-full w-full bg-green-700 rounded-full animate-ping" />
+            </div>
+            <span>{status}</span>
+          </motion.div>
+        )}
       </div>
-    );
-  }
-
-  return;
+    </AnimatePresence>
+  );
 }
