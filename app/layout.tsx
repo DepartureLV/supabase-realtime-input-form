884d592295020d4242f8360a5b0a2d35a46e0d83
Nattawat Pitikomon <nattawat.arch@gmail.com>
2025-03-30 11:36:48 +0700

    UI: improve

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             {/* NAV */}
             <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
               <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                <div className="flex gap-5 items-center font-semibold">
+                <div className="flex gap-5 items-center font-semibold text-xl text-primary hover:text-primary/80 transition-all duration-500">
                   <Link href={"/"}>Agnos Assignment</Link>
                 </div>
                 <div className="flex gap-4">
